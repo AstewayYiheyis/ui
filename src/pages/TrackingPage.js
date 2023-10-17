76c9acd6a64ff621330ce2864093285521d34eae
Asteway <90155375+AstewayYiheyis@users.noreply.github.com>
2023-10-17 02:50:37 -0900

    add styles to tracking page

--- src/pages/TrackingPage.js
+++ src/pages/TrackingPage.js
@@ -1,9 +1,14 @@
-import TextField from "@mui/material/TextField";
+import {TextField, Button, Container} from "@mui/material";
 import "./TrackingPage.css";
 
 export function TrackingPage(props){
     return <div id="tracking-page">
-        <label>Please Enter The Tracking Number Below:</label>
-        <TextField id="tracking-number" label="Tracking Number" variant="standard" />
+        <div id="tracking-box">
+            <h2>Please Enter The Tracking Number Below:</h2>
+            <div className="flex-container">
+                <TextField id="tracking-number" label="Tracking Number" variant="standard" />
+                <Button variant="contained">Track</Button>
+            </div>
+        </div>
     </div>;
 }
