4766bf0a7a3570e6796d85bd94021e376d1ebdce
Asteway <90155375+AstewayYiheyis@users.noreply.github.com>
2023-10-25 07:18:49 -0900

    tracking input and shipping client added

---
+++ src/components/TrackingInputContainer.js
@@ -0,0 +1,26 @@
+import {TextField, Button} from "@mui/material";
+import "./TrackingInputContainer.css";
+import {ShippingClient} from "../common_util_functions/ShippingClient";
+import {useState} from "react";
+
+export function TrackingInputContainer(props){
+    const [trackingNumber, setTrackingNumber] = useState(0);
+    const [shippmentInformation, setShippmentInformation] = useState(null);
+
+    function searchUsingTrackingNumber(event) {
+        event.preventDefault();
+        setShippmentInformation(ShippingClient.trackPackage(trackingNumber));
+        console.log(shippmentInformation);
+    }
+
+    function handleChange(event){
+        setTrackingNumber(event.target.value);
+    }
+
+    return(
+        <div className="flex-container">
+        <TextField id="tracking-number" label="Tracking Number" variant="standard" onChange={handleChange}/>
+        <Button variant="contained" onClick={searchUsingTrackingNumber}>Track</Button>
+        </div>
+    );
+}
--- src/pages/TrackingPage.js
+++ src/pages/TrackingPage.js
@@ -1,14 +1,11 @@
-import {TextField, Button, Container} from "@mui/material";
 import "./TrackingPage.css";
+import {TrackingInputContainer} from "../components/TrackingInputContainer";
 
 export function TrackingPage(props){
     return <div id="tracking-page">
         <div id="tracking-box">
             <h2>Please Enter The Tracking Number Below:</h2>
-            <div className="flex-container">
-                <TextField id="tracking-number" label="Tracking Number" variant="standard" />
-                <Button variant="contained">Track</Button>
-            </div>
+            <TrackingInputContainer/>
         </div>
     </div>;
 }
